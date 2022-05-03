0d9e20634e390e37625c30f9c534ce8ed1345097
tolaa <mgamtaa@gmail.com>
2022-05-03 15:06:49 +0300

    static pages

---
+++ web_static/images/Welcome to MySQLdb’s documentation! — MySQLdb 1.2.4b4 documentation_files/readthedocs-data.js
@@ -0,0 +1,20 @@
+var READTHEDOCS_DATA = {
+    project: "mysqlclient",
+    version: "latest",
+    language: "en",
+    programming_language: "py",
+    subprojects: {},
+    canonical_url: "https://mysqlclient.readthedocs.io/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/doc/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    proxied_api_host: "https://readthedocs.org",
+    commit: "6c67620b",
+    ad_free: false,
+
+    global_analytics_code: "UA-17997319-1",
+    user_analytics_code: null
+};
+
